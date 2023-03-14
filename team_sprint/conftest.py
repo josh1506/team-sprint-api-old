28da39e67043d252d8d9bf22dfcd6b4a827e14db
josh1506 <jmjabor@gmail.com>
2023-03-14 19:16:55 +0800

    Initial project setup

---
+++ team_sprint/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from team_sprint.users.models import User
+from team_sprint.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
